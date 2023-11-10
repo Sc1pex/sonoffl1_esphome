cf7911c04589cb56b647647751fcad32eb44dae5
Sc1pex <scipex.dev@gmail.com>
2023-11-10 13:58:34 +0200

    a

--- esphome/components/sonoff_l1/sonoff_l1.cpp
+++ esphome/components/sonoff_l1/sonoff_l1.cpp
@@ -8,17 +8,36 @@ namespace sonoff_l1 {
 static const char *const TAG = "components.l1";
 
 void SonoffL1::write_state(light::LightState *state) {
-  strcpy(wbuff,
-         "AT+UPDATE=\"sequence\":\"1\",\"switch\":\"on\",\"light_type\":1,\"colorR\":255,\"colorG\":20,\"colorB\":100,"
-         "\"bright\":100,\"mode\":1");
+  // This will be called by the light to get a new state to be written.
+  float red, green, blue;
+  // use any of the provided current_values methods
+  state->current_values_as_rgb(&red, &green, &blue);
 
-  Serial.print(wbuff);
-  Serial.write(0x1b);
+  // Convert to 0-255
+  int redValue, greenValue, blueValue;
+  redValue = floor(red * 255);
+  greenValue = floor(green * 255);
+  blueValue = floor(blue * 255);
+
+  bool ledState;
+  state->current_values_as_binary(&ledState);
+
+  float brightnessPercent;
+  state->current_values_as_brightness(&brightnessPercent);
+
+  // Convert to 0-100
+  int brightness = floor(brightnessPercent * 100);
+
+  char buffer[140];
+  snprintf_P(buffer, sizeof(buffer),
+             PSTR("AT+UPDATE=\"sequence\":\"%d%03d\",\"switch\":\"%s\",\"light_type\":1,\"colorR\":%d,\"colorG\":%d,"
+                  "\"colorB\":%d,\"bright\":%d,\"mode\":%d"),
+             millis(), millis() % 1000, ledState ? "on" : "off", redValue, greenValue, blueValue, brightness, 1);
+
+  Serial.print(buffer);
+  Serial.write(0x1B);
   Serial.flush();
-  ESP_LOGD(TAG, "Wrote %s", wbuff);
-
-  Serial.readBytesUntil(0x1b, rbuff, 1024);
-  ESP_LOGD(TAG, "Recv: %s", rbuff);
+  ESP_LOGD(TAG, "Wrote: %s", buffer);
 }
 
 void SonoffL1::setup() { Serial.begin(19200); }
